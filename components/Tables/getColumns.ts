8a4d2cc796eeefd0233976dbf0141c9487a5adc3
Sal Castoro <salcastoro@gmail.com>
2022-05-01 09:07:29 -0400

    Continue getColumns() refactor
    Add reduce function to getColumns function to generate headers array.
    Reduce function allows for collecting of all unique key: value pairs
    within the objects in units parameter array. Loop through every object
    and check if header object has been created in the accumulator. If not,
    add it else skip to next iteration.

--- components/Tables/getColumns.ts
+++ components/Tables/getColumns.ts
@@ -1,11 +1,21 @@
 import { Column } from 'react-table';
+import { TableUnit } from 'types/types';
 
-export function getColumns(row: object[]): Column[] {
-  row.sort(function (a, b) {
-    return Object.keys(b).length - Object.keys(a).length;
-  });
-  return Object.keys(row[0]).map((key) => ({
-    Header: key,
-    accessor: key,
-  }));
+export function getColumns(units: TableUnit[]): Column[] {
+  return units.reduce((acc: Column[], curr) => {
+    // Use reduce to loop through every object in units parameter array
+    Object.keys(curr).forEach((key) => {
+      // Check if object has been created in accumulator based off of the current key
+      if (!acc.find((item) => item.Header === key && item.accessor === key)) {
+        // If not, add it to accumulator array
+        acc.push({
+          Header: key,
+          accessor: key,
+        });
+      }
+      // Else, continue to next iteration
+    });
+    return acc;
+    // Return array of header objects containing all unique date keys
+  }, []);
 }
