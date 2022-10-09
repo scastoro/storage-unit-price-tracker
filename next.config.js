6be2b5e85dd9d26b01b989f550586ab2fc4edb16
Sal Castoro <salcastoro@gmail.com>
2022-10-09 10:59:29 -0400

    more cors 2

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: `http://${process.env.NEXT_PUBLIC_VERCEL_URL}/*`},
+          { key: "Access-Control-Allow-Origin", value: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/*`},
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
