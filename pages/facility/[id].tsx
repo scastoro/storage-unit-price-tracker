db5e6f4514133ce0353a336c7aca7ad838a37c33
Sal Castoro <salcastoro@gmail.com>
2022-10-15 11:01:36 -0400

    Add DatePicker & date query params

--- pages/facility/[id].tsx
+++ pages/facility/[id].tsx
@@ -22,6 +22,8 @@ import UnitTable from 'components/Tables/UnitTable';
 import { getColumns } from 'components/Tables/getColumns';
 import { Column } from 'react-table';
 import { formatUnitsDate } from 'utils/formatUnitsDate';
+import { sub, format } from 'date-fns';
+import DatePicker from 'react-datepicker';
 
 const Facility: NextPage = () => {
   ChartJS.register(
@@ -35,6 +37,7 @@ const Facility: NextPage = () => {
     Legend
   );
   const [loading, setLoading] = useState(false);
+  const [date, setDate] = useState(format(sub(new Date(), { months: 4 }), 'yyyy-MM-dd'));
   const [formattedUnits, setFormattedUnits] = useState<UnitFormat[]>([]);
   const [tableUnits, setTableUnits] = useState<TableUnit[]>([]);
   const [unitColumns, setUnitColumns] = useState<Column[]>([]);
@@ -48,7 +51,7 @@ const Facility: NextPage = () => {
   useEffect(() => {
     setLoading(true);
     async function getUnits() {
-      const response = await fetch(`/api/units?facility=${id}`, {
+      const response = await fetch(`/api/units?facility=${id}&createdAt>${date}&sort=-createdAt`, {
         mode: 'cors',
       });
       const units = await response.json();
@@ -56,8 +59,9 @@ const Facility: NextPage = () => {
     }
     if (id) {
       getUnits();
+      console.log(date);
     }
-  }, [id, dispatch]);
+  }, [id, dispatch, date]);
 
   useEffect(() => {
     if (units) {
@@ -74,32 +78,38 @@ const Facility: NextPage = () => {
   }, [tableUnits]);
 
   return (
-    <section className='m-auto w-4/5'>
-      <h1 className='text-3xl underline mb-5'>
-        {facilities.find((facility) => facility._id === id)?.name}
-      </h1>
-      <section className='table-container mb-10'>
-        {!loading && <UnitTable units={tableUnits} tableColumns={unitColumns} />}
-      </section>{' '}
-      {!loading && (
-        <section style={{ width: '65%', marginBottom: '30px' }} className='chart-container'>
-          <Line
-            options={{
-              responsive: true,
-              scales: {
-                y: {
-                  type: 'logarithmic',
-                  ticks: {},
+    <>
+      <DatePicker
+        selected={new Date(date)}
+        onChange={(date: Date) => setDate(format(date, 'yyyy-MM-dd'))}
+      />
+      <section className='m-auto w-4/5'>
+        <h1 className='text-3xl underline mb-5'>
+          {facilities.find((facility) => facility._id === id)?.name}
+        </h1>
+        <section className='table-container mb-10'>
+          {!loading && <UnitTable units={tableUnits} tableColumns={unitColumns} />}
+        </section>{' '}
+        {!loading && (
+          <section style={{ width: '65%', marginBottom: '30px' }} className='chart-container'>
+            <Line
+              options={{
+                responsive: true,
+                scales: {
+                  y: {
+                    type: 'logarithmic',
+                    ticks: {},
+                  },
                 },
-              },
-            }}
-            data={{
-              datasets: formattedUnits,
-            }}
-          />
-        </section>
-      )}
-    </section>
+              }}
+              data={{
+                datasets: formattedUnits,
+              }}
+            />
+          </section>
+        )}
+      </section>
+    </>
   );
 };
 
